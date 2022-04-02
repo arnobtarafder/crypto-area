cdf3aa49e9cc54cccbfe19f06d39fa692cef6e75
arnobtarafder <rmtarnob@gmail.com>
2022-04-03 00:14:42 +0600

    logo

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -6,7 +6,7 @@ const Header = () => {
     return (
         <div className='sticky top-0 bg-white px-11 py-7 flex md:justify-between'>
             <div className='hidden md:block'>
-                <Link className='font-sans font-bold text-xl text-zinc-600' to='/'>Crypto Area</Link>
+                <Link className='font-sans font-bold text-xl text-zinc-600' to='/'>Crypto Arena</Link>
                 </div>
 
             <div className='flex gap-5 text-xl'>
